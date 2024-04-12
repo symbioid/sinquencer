0e469ad762d4611d72f67893dd7d754f28794dbf
symbioid <symbioid@gmail.com>
2024-04-12 13:05:32 -0500

    complete: Main Window + Center Line TODO/In progress: Generating Sin Wave

--- sinquencer.py
+++ sinquencer.py
@@ -1,6 +1,6 @@
-# Example file showing a circle moving on screen
-import pygame
 import math
+
+import pygame
 
 # pygame setup
 # pygame.init()
@@ -12,12 +12,29 @@ dt = 0
 player_pos = pygame.Vector2(screen.get_width() * 0.5, screen.get_height() * 0.5)
 x_offset = screen.get_width() * 0.0625
 y_offset = screen.get_height() * 0.0833
-window_width = screen.get_width() - x_offset * 2.0
+window_width = int(screen.get_width() - x_offset * 2.0)
 window_height = screen.get_height() * 0.625 - y_offset
 num_samples = window_width
-wave = [[x for x in math.asin(1)]]
-for x in wave:
-    print(wave)
+sample_rate = num_samples
+wave = [num_samples]
+
+
+# (Math.sin ( Tau * waves_frequency * x /
+#  sampleRate * timeScale) *
+#  waves[w].amplitude)
+#
+
+
+for x in range(window_width):
+    # timeScale = ( window_width/numSamples );
+    # currentY = ( Math.asin(math.tau * waves_frequency * x / sampleRate * timeScale* waves[w].amplitude)
+    current_y = math.asin(((math.radians(2.0) * x / num_samples) * 1000) / 1000)
+    wave.append(current_y)
+
+print(list(enumerate(wave)))
+
+# for x in wave
+#    print(wave[x])
 
 
 def draw_window(x, y, w, h):
@@ -29,13 +46,10 @@ def draw_center_line(x, y, x2, y2):
 
 
 while running:
-
-    # poll for events - pygame.QUIT = user clicked X to close window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
 
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
     draw_window(x_offset, y_offset, window_width, window_height)
     draw_center_line(
@@ -45,7 +59,7 @@ while running:
         y_offset + window_height / 2,
     )
 
-    # flip() the display to put your work on screen
+    # flip() buffers
     pygame.display.flip()
 
     # limits FPS to 60
