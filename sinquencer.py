1d5e43b403eeaccd96fc9fd2754907b0a6c002ef
symbioid <symbioid@gmail.com>
2024-04-16 20:01:43 -0500

    draw 1 sub-window/channel.

--- sinquencer.py
+++ sinquencer.py
@@ -15,18 +15,42 @@ x_offset = screen.get_width() * 0.0625
 y_offset = screen.get_height() * 0.0833
 window_width = int(screen.get_width() - x_offset * 2.0)
 window_height = screen.get_height() * 0.625 - y_offset
-num_samples = window_width
-sample_rate = num_samples
+sample_rate = 44000
+num_samples = 44000
 wave = [num_samples]
 waves_frequency = 2.0
 amplitude = 100
 
+waves_array = [
+    {"frequency": 1, "amplitude": 200},
+    {"frequency": 2, "amplitude": 22},
+    {"frequency": 3, "amplitude": 22},
+    {"frequency": 4, "amplitude": 22},
+    {"frequency": 5, "amplitude": 22},
+    {"frequency": 6, "amplitude": 22},
+    {"frequency": 7, "amplitude": 22},
+    {"frequency": 8, "amplitude": 22},
+    {"frequency": 9, "amplitude": 22},
+]
+
+channels_array = [
+    {
+        "x_off": x_offset,
+        "y_off": y_offset + window_height + 20,
+        "width": window_width / 8,
+        "height": window_height / 8,
+        "color": "red",
+    }
+]
+
 
 def gen_wave():
     for x in range(window_width):
-        time_scale = window_width / num_samples
+        time_scale = sample_rate / window_width
         current_y = (
-            -math.sin(math.tau * waves_frequency * x / sample_rate * time_scale)
+            -math.sin(
+                math.tau * waves_array[3]["frequency"] * x / sample_rate * time_scale
+            )
             * amplitude
         )
         wave.append(current_y)
@@ -35,22 +59,37 @@ def gen_wave():
 def draw_wave():
     # pygame.draw.line(screen, "green", (0, 400), (1024, 400), 3)
     for i, x in enumerate(wave):
-        # if i == 0:
-        #    pass
+        if i == 0:
+            pass
         if i > 1:
             pygame.draw.line(
                 screen,
                 "orange",
                 ((i - 1 + x_offset), (wave[i - 1] + y_offset + (window_height / 2))),
                 ((i + x_offset), (wave[i] + y_offset + (window_height / 2))),
-                10,
+                3,
             )
 
         # print(list(enumerate(wave)), end="\n")
 
 
 def draw_window(x, y, w, h):
-    pygame.draw.rect(screen, "grey", (x, y, w, h), 3)
+    pygame.draw.rect(screen, "darkgrey", (x - 3, y - 3, w + 6, h + 6), 1)
+
+
+def draw_windows():
+    pygame.draw.rect(
+        screen,
+        channels_array[0]["color"],
+        (
+            channels_array[0]["x_off"] - 1,
+            channels_array[0]["y_off"] - 1,
+            channels_array[0]["width"] + 2,
+            channels_array[0]["height"] + 2,
+        ),
+        1,
+    )
+    # pygame.draw.line(screen, "grey", (x, y), (x2, y2), 1)
 
 
 def draw_center_line(x, y, x2, y2):
@@ -72,6 +111,7 @@ while running:
         x_offset + window_width,
         y_offset + window_height / 2,
     )
+    draw_windows()
 
     # flip() buffers
     pygame.display.flip()
