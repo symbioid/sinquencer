e7add23c068de25c7f9ad51ab812372bccfb6bea
symbioid <symbioid@gmail.com>
2024-05-04 23:54:37 -0500

    draw main window/rect

--- sinquencer.py
+++ sinquencer.py
@@ -2,8 +2,6 @@ import math
 import pygame
 import pygame.gfxdraw
 
-pygame.init()
-
 screen = pygame.display.set_mode((1280, 960))  # (1024, 768))  #
 clock = pygame.time.Clock()
 running = True
@@ -12,23 +10,31 @@ x_offset = screen.get_width() * 0.0625
 y_offset = screen.get_height() * 0.0833
 window_width = int(screen.get_width() - x_offset * 2.0)
 window_height = screen.get_height() * 0.625 - y_offset
-sample_rate = screen.get_width()
-num_samples = sample_rate
+sample_rate = 44000  # screen.get_width()
+num_samples = screen.get_width()
 wave = []
 tau = math.pi * 2
-frequency = 2.0
+frequency = 1.0
+amplitude = 100
+main_window = pygame.Rect(x_offset, y_offset, window_width, window_height)
+
+
+def draw_window():
+    pygame.gfxdraw.rectangle(screen, main_window, pygame.Color("red"))
 
 
 def gen_wave():
     for x in range(num_samples):
         time_scale = sample_rate / num_samples
-        current_y = -math.sin(tau * frequency * x / sample_rate * time_scale) * 100
+        current_y = (
+            -math.sin(tau * frequency * x / sample_rate * time_scale) * amplitude
+        )
         print(current_y)
         wave.append(current_y)
 
 
 def draw_waves(wave):  # noqa: ANN001
-    for x in range(sample_rate - 1):
+    for x in range(num_samples - 1):
         pygame.draw.line(
             screen,
             pygame.Color("grey"),
@@ -46,5 +52,6 @@ gen_wave()
 while running:
     for event in pygame.event.get():
         screen.fill("black")
+        draw_window()
         draw_waves(wave)
         pygame.display.flip()
