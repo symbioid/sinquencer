18433ed34d79000215fc02144c977bceab7be6b5
symbioid <symbioid@gmail.com>
2024-07-08 14:39:32 -0500

    Added Rect for "main screen" (to render all sub-waves)

--- sinquencer.py
+++ sinquencer.py
@@ -14,38 +14,37 @@ x_offset = screen.get_width() * 0.0625
 y_offset = screen.get_height() * 0.0833
 window_width = int(screen.get_width() - x_offset * 2.0)
 window_height = screen.get_height() * 0.625 - y_offset
-sample_rate = screen.get_width()
-num_samples = sample_rate
+sample_rate = 1
+num_samples = screen.get_width()  # sample_rate
 wave = []
 tau = math.pi * 2
-frequency = 10.0
-vcenter = screen.get_width() / 2
-ycenter = screen.get_height() / 2
+frequency = 5.5
+main_window = pygame.Rect(x_offset, y_offset, window_width, window_height)
+amplitude = 100
 
 
-def gen_wave():
+def gen_wave(rec):
     for x in range(num_samples):
-        time_scale = screen.get_width() / sample_rate
-        current_y = -math.sin(tau * frequency * x / sample_rate * time_scale) * 100
+        # time_scale = rec.width
+        current_y = -math.sin(tau * frequency * x / rec.width) * amplitude
         print(current_y)
         wave.append(current_y)
 
 
-def draw_waves(wave):
-    for x in range(screen.get_width() - 1):
-        pygame.draw.line(
+def draw_wave(wave, rec):
+    for x in range(1, rec.width):
+        pygame.draw.circle(
             screen,
             pygame.Color("red"),
-            (x, wave[x] + ycenter),
-            (x + 1, wave[x + 1] + ycenter),
+            (rec.left + x, wave[x] + rec.centery),
+            1.0,
         )
 
 
 print(list(enumerate(wave)), end="\n")
 
-
 # if __name__ == "__Main__":
-gen_wave()
+gen_wave(main_window)
 
 while running:
     for event in pygame.event.get():
@@ -56,8 +55,10 @@ while running:
         pygame.draw.line(
             screen,
             pygame.Color("grey"),
-            (0, ycenter),
-            (screen.get_width(), ycenter),
+            (main_window.left - 1, main_window.centery),
+            (main_window.right - 1, main_window.centery),
+            1,
         )
-        draw_waves(wave)
+        pygame.draw.rect(screen, pygame.Color("grey"), main_window, 1)
+        draw_wave(wave, main_window)
         pygame.display.flip()
