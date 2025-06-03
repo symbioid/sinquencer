77ccadd814bb7611dbea5f3fe1b246dcf9109f08
symbioid <symbioid@gmail.com>
2025-06-02 20:48:35 -0500

    render all 7 waves
    -render all 7 waves to sub_windows
    -comment out dataclass (not used yet)
    -updated colors list
    TODO:
    -Fix rendering to be based on scaling using a sample rate of 44000.
    (currently is based on window size)
    -once above is complete, render all waves to main_window
    -add x segments (steps) to main_window.
    -select active/working waves
    -calculate crossings of selected waves
    -highlight the active step (in which the crossings happen)
    -implement midi triggering
    -implement timer and playhead to scan across main window and trigger
    highlighted/active steps

--- sinquencer.py
+++ sinquencer.py
@@ -18,16 +18,16 @@ main_y_offset = 80
 main_window_width = screen.get_width() - 160
 main_window_height = screen.get_height() * 0.625 - main_y_offset
 
-small_window_width = 70
-small_window_height = 20
+sub_window_width = 70
+sub_window_height = 20
 colors = [
-    pygame.Color("red"),
-    pygame.Color("orange"),
-    pygame.Color("yellow"),
-    pygame.Color("green"),
-    pygame.Color("blue"),
-    pygame.Color("indigo"),
-    pygame.Color("violet"),
+    pygame.Color("red3"),
+    pygame.Color("orange3"),
+    pygame.Color("yellow3"),
+    pygame.Color("green3"),
+    pygame.Color("dodgerblue2"),
+    pygame.Color("slateblue3"),
+    pygame.Color("violetred3"),
 ]
 
 main_window = pygame.Rect(
@@ -36,115 +36,127 @@ main_window = pygame.Rect(
     main_window_width,
     main_window_height,
 )
-small_windows = [
-    # RED 1
+sub_windows = [
+    # RED 0
     pygame.Rect(
         int(main_x_offset),
         int(main_y_offset + main_window_height + 50),
-        int(main_x_offset + small_window_width),
-        int(main_y_offset + small_window_height),
+        int(main_x_offset + sub_window_width),
+        int(main_y_offset + sub_window_height),
     ),
-    # ORANGE 2
+    # ORANGE 1
     pygame.Rect(
-        int(main_x_offset + small_window_width * 2 + 22),
+        int(main_x_offset + sub_window_width * 2 + 22),
         int(main_y_offset + main_window_height + 50),
-        int(main_x_offset + small_window_width),
-        int(main_y_offset + small_window_height),
+        int(main_x_offset + sub_window_width),
+        int(main_y_offset + sub_window_height),
     ),
-    # YELLOW 3
+    # YELLOW 2
     pygame.Rect(
-        int(main_x_offset + small_window_width * 4 + 44),
+        int(main_x_offset + sub_window_width * 4 + 44),
         int(main_y_offset + main_window_height + 50),
-        int(main_x_offset + small_window_width),
-        int(main_y_offset + small_window_height),
+        int(main_x_offset + sub_window_width),
+        int(main_y_offset + sub_window_height),
     ),
-    # GREEN 4
+    # GREEN 3
     pygame.Rect(
-        int(main_x_offset + small_window_width * 6 + 66),
+        int(main_x_offset + sub_window_width * 6 + 66),
         int(main_y_offset + main_window_height + 50),
-        int(main_x_offset + small_window_width),
-        int(main_y_offset + small_window_height),
+        int(main_x_offset + sub_window_width),
+        int(main_y_offset + sub_window_height),
     ),
-    # BLUE 5
+    # BLUE 4
     pygame.Rect(
-        int(main_x_offset + small_window_width * 8 + 88),
+        int(main_x_offset + sub_window_width * 8 + 88),
         int(main_y_offset + main_window_height + 50),
-        int(main_x_offset + small_window_width),
-        int(main_y_offset + small_window_height),
+        int(main_x_offset + sub_window_width),
+        int(main_y_offset + sub_window_height),
     ),
-    # INDIGO 6
+    # INDIGO 5
     pygame.Rect(
-        int(main_x_offset + small_window_width * 10 + 110),
+        int(main_x_offset + sub_window_width * 10 + 110),
         int(main_y_offset + main_window_height + 50),
-        int(main_x_offset + small_window_width),
-        int(main_y_offset + small_window_height),
+        int(main_x_offset + sub_window_width),
+        int(main_y_offset + sub_window_height),
     ),
-    # VIOLET 7
+    # VIOLET 6
     pygame.Rect(
-        int(main_x_offset + small_window_width * 12 + 132),
+        int(main_x_offset + sub_window_width * 12 + 132),
         int(main_y_offset + main_window_height + 50),
-        int(main_x_offset + small_window_width),
-        int(main_y_offset + small_window_height),
+        int(main_x_offset + sub_window_width),
+        int(main_y_offset + sub_window_height),
     ),
 ]
 
 tau = math.pi * 2
 frequency = 1.0
-amplitude = 150
+amplitude = sub_window_height
 phase = 0
-sample_rate = main_window.width
+#sample_rate = sub_window_width + 80
+sample_rate = sub_window_width + 80
 num_samples = sample_rate
 
 
 @dataclass
-class wave():
+class wave:
     wave_data: list
     freq: float = 0.0
-    amp: float = small_window_height * .289
+    amp: float = sub_window_height
     phase: float = 0.0
 
-def gen_wave(window: pygame.Rect, frequency: float, phase: float) -> list:
+
+waves = []
+
+
+def gen_wave(frequency: float, phase: float) -> list:
     wave = []
     for x in range(num_samples):
-        current_y = math.sin(tau * x * frequency /
-                             sample_rate + phase) * amplitude
+        current_y = math.sin(tau * x * frequency / sample_rate + phase) * amplitude
         logger.info(current_y)
         wave.append(current_y)
     return wave
 
-def all_waves() -> list:
-    g = []
-    for i in range(6):
-        g.append(gen_wave(main_window, 1, i))
-    return g
 
-def draw_wave(wave: list, color: pygame.Color):
+def all_waves() -> list:
+    # g = []
+    for i in range(7):
+        waves.append(gen_wave(i + 1, 0))
+    return waves
+
+
+def draw_wave(window: pygame.Rect, wave: list, color: pygame.Color):
     # (instead scale wave to window, dont chop off) !!!
     # for x in range(1, num_samples):
-    for x in range(2, num_samples):
+    for x in range(0, num_samples):
         pygame.draw.circle(  # dot to plot the wave
             screen,
             color,
-            (main_window.left + x, -wave[x] + main_window.centery),
+            (window.left + x, wave[x] + window.centery),
             1.0,
         )
+
 
 def draw_all_waves(all_waves: list):
     # draw on main screen
     for i, wave in enumerate(all_waves):
-        draw_wave(wave, colors[i])
+        draw_wave(sub_windows[i], wave, colors[i])
+
 
 def draw_sub_windows() -> None:
-    for i, window in enumerate(small_windows):
+    for i, window in enumerate(sub_windows):
+        """Drawing the sub-windows"""
         pygame.draw.rect(
             screen,
             colors[i],
             window,
             1,
         )
+
+        """Drawing center lines"""
         pygame.draw.lines(
             screen,
-            pygame.Color("grey"),  # colors[i],
+            colors[i],
+            #pygame.Color("grey"),  # colors[i],
             False,
             (
                 (window.left - 1, window.centery),
@@ -152,6 +164,7 @@ def draw_sub_windows() -> None:
             ),
             1,
         )
+
 
 def draw_main_window() -> None:
     pygame.draw.line(
@@ -165,6 +178,7 @@ def draw_main_window() -> None:
 
 
 waves = all_waves()
+
 
 while running:
     for event in pygame.event.get():
